80edbcf129c1b5e982c4fc59502ea0bd19ebe925
cheefung97 <shuaigeomg@gmail.com>
2023-10-01 22:46:12 +0800

    cf

---
+++ src/components/Header.svelte
@@ -0,0 +1,13 @@
+<div class="bg-neutral text-neutral-content">
+    <div class="max-w-l mx-auto navbar">
+        <div class="navbar start">
+            <a href="/" class="btn btn-ghost text-xl">Home</a>
+        </div>
+        <div class="navbar-end">
+            <ul class="menu menu-horizontal">
+                <li><a href="/login">Login</a></li>
+                <li><a href="/register">Register</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
